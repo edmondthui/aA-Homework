8475c85bd9a1a1adbf8a668e04e95265c8c13e2c
Edmond Hui <edmond.t.hui@gmail.com>
2020-09-21 23:03:51 -0400

    :white_check_mark: complete W4D2 homework working mancala game :white_check_mark:

--- W4D2/lib/board.rb
+++ W4D2/lib/board.rb
@@ -1,17 +1,22 @@
 class Board
   attr_accessor :cups
 
+  attr_reader :name1, :name2
+
   def initialize(name1, name2)
-    @cups = Array.new(14, [:stone, :stone, :stone, :stone])
-    @cups.each_with_index do |cup, i|
-      if i == 6 || i == 13
-        @cups[i] = []
-      end
-    end
+    @cups = Array.new(14) {Array.new()}
+    @name1 = name1
+    @name2 = name2
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      next if i == 6 || i == 13
+      4.times do
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
@@ -20,10 +25,34 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones_in_cup = @cups[start_pos]
+    @cups[start_pos] = []
+    i = start_pos
+    
+    while !stones_in_cup.empty?
+      i += 1
+      i = 0 if i > 13
+      if current_player_name == @name1
+        next if i == 13
+        @cups[i] << stones_in_cup.shift
+      elsif current_player_name == @name2 
+        next if i == 6
+        @cups[i] << stones_in_cup.shift
+      else
+        @cups[i] << stones_in_cup.shift
+      end
+    end
+
+    render
+    next_turn(i)
+
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    return :prompt if ending_cup_idx == 6 || ending_cup_idx == 13
+    return :switch if @cups[ending_cup_idx].length == 1
+    ending_cup_idx
   end
 
   def render
@@ -35,8 +64,18 @@ class Board
   end
 
   def one_side_empty?
+    return true if @cups[1..6].all? {|cup| cup == []}
+    return true if @cups[7..12].all? {|cup| cup == []}
+    false
   end
 
   def winner
+    if @cups[6].length == @cups[13].length
+      return :draw
+    elsif @cups[6].length > @cups[13].length
+      return name1
+    else
+      return name2
+    end
   end
 end
